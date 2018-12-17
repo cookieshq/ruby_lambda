169bdf03186462d39369071b9f6e474d19988103
denissellu <hingasellu@gmail.com>
2018-12-17 19:29:59 +0000

    WIP adding a way to test the function through invoking it

--- lib/ruby_lambda.rb
+++ lib/ruby_lambda.rb
@@ -1,9 +1,11 @@
+require 'json'
 require 'thor'
 require 'fileutils'
 require 'awesome_print'
 
 require 'ruby_lambda/version'
 require 'ruby_lambda/init'
+require 'ruby_lambda/test'
 
 module RubyLambda
 end
---
+++ lib/ruby_lambda/test.rb
@@ -0,0 +1,21 @@
+require 'ruby_lambda/lambda_context'
+
+module RubyLambda
+  class Test
+    def initialize(current_directory)
+      @current_directory  = current_directory
+      @shell = Thor::Base.shell.new
+    end
+
+    def run
+      load "#{@current_directory}/main.rb"
+
+      event_json_file = File.read('event.json')
+      event = JSON.parse(event_json_file)
+
+      context = LambdaContext.new()
+
+      ap lambda_handler(event: event, context: context)
+    end
+  end
+end
