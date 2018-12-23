783e07976789567a0ecf8196a32ff98671f82822
denissellu <hingasellu@gmail.com>
2018-12-23 14:09:25 +0000

    Add config for simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,15 @@
-require "bundler/setup"
-require "ruby_lambda"
+require 'bundler/setup'
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter '/spec/'
+end
+
+require 'ruby_lambda'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
